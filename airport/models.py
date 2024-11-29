bd2e986bd6c8c3f9343097c6c8f44228ce18d5f1
antonver <anton.verbovyi@gmail.com>
2024-11-29 18:04:16 +0100

    feat(db): Improve Ticket model validation
    Add validation to Ticket model to ensure seat uniqueness
    within a flight and prevent invalid seat entries based on
    airplane configuration.  This prevents booking seats that
    don't exist and ensures data integrity.

--- airport/models.py
+++ airport/models.py
@@ -1,6 +1,10 @@
+import string
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import UniqueConstraint
+from rest_framework.exceptions import ValidationError
 
 from airpot_api import settings
 
@@ -32,9 +36,35 @@ class Crew(models.Model):
 
 class Ticket(models.Model):
     row = models.IntegerField()
-    seat = models.IntegerField()
+    seat = models.CharField(max_length=5)
     flight = models.ForeignKey("Flight", on_delete=models.CASCADE, related_name="tickets")
     order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="tickets")
+
+    class Meta:
+        unique_together = ("seat", "flight", "row")
+
+    @staticmethod
+    def validate_tickets(seat, row, seats_in_row, rows, error_to_rise):
+        possible_seats_in_row = [list(string.ascii_uppercase)[i] for i in range(seats_in_row)]
+        if (seat[-1] not in possible_seats_in_row or row != int(seat[:-1])
+                or row < 0 or row > rows):
+            raise error_to_rise(
+                {
+                    "seat": f"in the  plane {seats_in_row} seats in a row and {rows} rows"
+                }
+            )
+
+    def clean(self):
+        self.validate_tickets(self.seat,
+                              self.row,
+                              self.flight.airplane.seats_in_row,
+                              self.flight.airplane.rows,
+                              ValidationError)
+        super().clean()
+
+    def save(self, *args, **kwargs):
+        super().full_clean()
+        super().save(*args, **kwargs)
 
 
 class Order(models.Model):

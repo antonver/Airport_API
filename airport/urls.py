7088b64827cc712f6b59de4fa6be4c6ccc660292
antonver <anton.verbovyi@gmail.com>
2024-12-08 01:40:51 +0100

    feat(api): Replace TicketViewSet with TicketList
    Refactored ticket endpoint to use a generic list view.  This
    simplifies the code and improves maintainability.

--- airport/urls.py
+++ airport/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from rest_framework import routers
 
 from airport.views import FlightViewSet, AirportViewSet, OrderViewSet, RouteViewSet, CrewViewSet, AirplaneViewSet, \
-    AirplaneTypeViewSet, TicketViewSet
+    AirplaneTypeViewSet, TicketList
 
-urlpatterns = [
-]
+urlpatterns = [path("tickets/", TicketList.as_view(), name="ticket-list"),]
 
 router = routers.DefaultRouter()
 router.register(r'flights', FlightViewSet)
@@ -15,7 +14,6 @@ router.register(r'routes', RouteViewSet)
 router.register(r'crews', CrewViewSet)
 router.register(r'airplanes', AirplaneViewSet)
 router.register(r'airplane_types', AirplaneTypeViewSet)
-router.register(r'tickets', TicketViewSet)
 urlpatterns += router.urls
 
 app_name = 'airport'
